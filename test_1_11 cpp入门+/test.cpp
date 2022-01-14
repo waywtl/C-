000844c4436118093a8104cd502b9a703724f1b9
waywtl <1148117782@qq.com>
2022-01-14 21:58:04 +0800

    cpp入门+

---
+++ test_1_11 cpp入门+/test.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+//引用权限缩小
+//int main()
+//{
+//	int a = 10;
+//	const int& b = a;
+//
+//	//b = 20;
+//	a = 20;
+//	cout << a << endl;
+//	return 0;
+//}
+
+//#define ADD(x,y) ((x)+(y))
+//int main()
+//{
+//	cout << ADD(1,2) << endl;
+//	return 0;
+//}
+
+//内联函数
+//inline int Add(int x, int y)
+//{
+//	int ret = x + y;
+//	return ret;
+//}
+//
+//int main()
+//{
+//	Add(1, 2);
+//	Add(1, 2);
+//	Add(1, 2);
+//	Add(1, 2);
+//	Add(1, 2);
+//	Add(1, 2);
+//	return 0;
+//}
